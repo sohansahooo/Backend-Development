9cde5a872777787d77bee06f476b4a2c8000cfc5
sohanxsahoo <116956354+sohanxsahoo@users.noreply.github.com>
2024-12-19 18:25:34 +0530

    create and config db models with aggregated pipeline

---
+++ 03_vidtube/src/models/user.model.js
@@ -0,0 +1,49 @@
+import { mongoose, Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      inndex: true,
+    },
+    avatar: {
+      type: String, //cloudinary URL
+    },
+    coverImage: {
+      type: String, //cloudinary URL
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model("User", userSchema);
